663165ecd9dfd418a10276a5ce069c4258a69752
Nguyễn Hòa Bình <binhbhgl5@gmail.com>
2023-09-03 21:02:22 +0700

    update title show

--- components/LinkItem.jsx
+++ components/LinkItem.jsx
@@ -9,9 +9,9 @@ function LinkItem(props) {
 			className="flex justify-between h-[40px]"
 		>
 			<div
-				className="flex items-center truncate w-1/2 max-lg:w-2/3 max-sm:w-4/5"
+				className="flex items-center w-1/2 max-lg:w-2/3 max-sm:w-4/5"
 			>
-				<a href={link.url}>{link.title}</a>
+				<a className='truncate' href={link.url}>{link.title}</a>
 			</div>
 			<Dropdown link={link} deleteLink={deleteLink}/>
 		</li>
